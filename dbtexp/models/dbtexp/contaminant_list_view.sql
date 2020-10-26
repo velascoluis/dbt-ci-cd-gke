6657183a95125fb32c620b936cf66c14dea91e0d
velascoluis <velascoluis@google.com>
2020-10-26 13:58:32 +0100

    Add new model file

--- dbtexp/models/dbtexp/contaminant_list_view.sql
+++ dbtexp/models/dbtexp/contaminant_list_view.sql
@@ -1,6 +1,7 @@
 {{ config(materialized='view') }}
 {% set contaminants = ["O3", "NO", "NO2"] %}
 SELECT
+
 {% for contaminat in contaminants %}
 sum(case when contaminant = '{{contaminat}}' then magnitud end) as {{contaminat}}_amount,
     {% endfor %}
